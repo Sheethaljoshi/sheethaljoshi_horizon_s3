87eb9c05bf808d590b02fc84e87ba9d178b8313e
Sheethal Joshi <sh33thal24@gmail.com>
2023-10-22 16:23:09 +0530

    added comments and readme documentation

--- 1.py
+++ 1.py
@@ -1,23 +1,35 @@
-count = 0
+#Here we initalise the 'count' to keep track of number of numbers being input and the 'tot' as the total sum of the numbers inputed , as both 0
+count = 0 
 tot = 0
-while True:
-    a=input("Enter a number to find ongoing average ( type 'stop' to  quit the program or restart to find new average): ")
-    if a.lower() == "stop":
-        break
+
+#This starts an infinite loop to repeatedly get the users input
+while True: 
     
+    #Ask the user to input a number, stop the program, or reset the average.
+    a=input("Enter a number to find ongoing average ( type 'stop' to  quit the program or 'restart' to find new average): ")
+    
+    #Check if the user wants to stop the program - the loop is terminated if condition for loop to break is satisfied.
+    if a.lower() == "stop":
+        break 
+    
+    #Check if the user wants to reset the average - the count snd total are rest to 0 and the rest of the loop is skipped.
     if a.lower() == "restart":
         count = 0
         tot = 0
         print("The average has been reset to 0")
         continue
-        
+    
+    #Check if the input represents a valid number (positive or negative, including decimals) and finds and prints the corresponding average
     if a.lstrip('-').replace('.', '', 1).isdigit():
         a=float(a)
         count += 1 
         tot += a 
         average=tot/count
         print("Current average: ", average)
-
+        
+    #If the input is not a valid number, displays an error message.
     else:
         print("Invalid input. Please try again.")
+        
+#Display a message when the user stops the program.
 print("The program has been stopped. Please run again to restart the program.")
--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# teamhorizon
+# Question 1
+
+Question: 
+In any programming language, write a code that constantly inputs a number and shows the average of all numbers inserted but does not accept a number that is repeated.
+
+Sample Input & Output:
+
+Input: 4
+Output: 4
+Input: 5
+Output: 4.5
+Input :5
+Output:4.5
+
+## How to Run the Program
+
+1. Clone or download the program from the GitHub repository to your local system.
+
+2. Ensure you have Python installed on your computer.
+
+3. Open a terminal or command prompt on your system.
+
+4. Navigate to the directory where you've stored the program files using the `cd` command.
+
+5. Run the program by entering the following command: python 1.py
+
+6. The program will start, and you'll be prompted to input numbers. You can type "stop" to exit the program or "restart" to reset the ongoing average to zero.
+
+7. To calculate the ongoing average, input valid numbers. The program will display the current average as you go.
+
+8. The program will terminate with a message when you decide to stop it.
+
+## Program Logic
+
+The program initializes two variables, `count` and `tot`, to keep track of the numbers entered and their sum. It uses a while loop to repeatedly take user input.
+
+- Input "stop" to exit the program.
+- Input "restart" to reset the ongoing average to zero.
+- Input valid numbers to calculate and display the ongoing average.
+- For invalid input, the program displays an error message and prompts for another input.
+
+Remember to type your inputs exactly as shown (case-insensitive) for the program to recognize them.
+
+**Happy calculating!**
