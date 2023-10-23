670058f3a770adc5d6265de7b343d106fca967fe
Sheethal Joshi <sh33thal24@gmail.com>
2023-10-23 15:36:47 +0530

    second

---
+++ 2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int n, i, j;
+    float dx, dy, direction;
+    float input[50][50];
+    float x = 0.0;
+    float y = 0.0;
+    cout<<"Enter the number of changes in motion";
+    cin>>n;
+    for(i=0;i<n;i++){
+        
+        cout<<"Enter velocity, direction (in degrees), and time in seconds for the change of motion - " << "("<<i+1<<")";
+   
+        for(j=0;j<3;j++){
+            cin>>input[i][j];
+        }
+
+        direction = (input[i][1] * M_PI)/180;
+
+        dx = input[i][0] * cos(direction) * input[i][2];
+        dy = input[i][0] * sin(direction) * input[i][2];
+
+
+
+
+        
+        
+    }
+}
